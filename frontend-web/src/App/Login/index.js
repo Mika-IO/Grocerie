d905fb7621f31e076f8a15527590f6049cfda7aa
Mika-IO <mikaio4321@gmail.com>
2020-10-25 19:40:21 -0400

    initial commit frontend-web

---
+++ frontend-web/src/App/Login/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+// import { Container } from './styles';
+
+function Login() {
+  return <div>
+    <Button/>
+  </div>;
+}
+
+export default Login;
---
+++ frontend-web/src/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Login from './Login';
+
+// import { Container } from './styles';
+
+function App() {
+  return <Login />;
+}
+
+export default App;
